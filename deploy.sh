9f2a258c772f8a78b24899b42bfa87633d786f03
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-11-15 12:16:16 +0000

    Add login

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,13 @@ serviceName=$1-$stage
 resourceGroupName=$serviceName"-rg"
 serviceNameToLower="$(echo $serviceName | tr '[:upper:]' '[:lower:]')"
 storageAccountName="$(echo ${serviceNameToLower//"-"/""})"
+echo "Creating resource group"
 az group create --name $resourceGroupName --location $location
+echo "Creating storage account"
 az storage account create --name $storageAccountName --location $location --resource-group $resourceGroupName --sku Standard_LRS
+echo "Creating function app"
 az functionapp create --name $serviceName --storage-account $storageAccountName --consumption-plan-location westeurope --resource-group $resourceGroupName
+echo "Publishing function locally"
+dotnet build
+echo "Publisihng function to Azure"
 func azure functionapp publish $serviceName
